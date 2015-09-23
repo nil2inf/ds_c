8a52a421232c543d8913fa9539f79d37e71ea77f
nil2inf <gnudennis@qq.com>
2015-09-23 17:15:10 +0800

    fork and change this file

--- calculate-expression-test.c
+++ calculate-expression-test.c
@@ -3,11 +3,14 @@
 
 #define MAX_LINE 300
 
+
+
 int 
 main(int argc, char **argv)
 {
 	char infixexp[MAX_LINE+1], postfixexp[MAX_LINE+1];
 	while ( gets(infixexp) ) {
+		
 		printf("expression: %s\n", infixexp);
 		if ( !balance_symbols(infixexp) )
 			printf("Fail to Convert infix to postfix\n");
